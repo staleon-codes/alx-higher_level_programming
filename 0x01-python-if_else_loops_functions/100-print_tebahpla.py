783a353da5ab6d69956d5d2f0fd18c4ed0214f33
staleon-codes <mbaselaleon@gmail.com>
2023-09-05 03:57:22 -0700

    Task 6-print_comb3.py fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format((65 + (i - 97))
-if (((123 - i) % 2) == 0) else i), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
