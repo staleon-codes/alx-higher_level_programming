0139b7d6f0a271afa41bfe01d2036957832b11de
staleon-codes <mbaselaleon@gmail.com>
2023-10-10 07:42:08 -0700

    task 7 fixed

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,30 +1,54 @@
 #!/usr/bin/python3
-"""Student module"""
+"""Module defining the class Student based on 10-student.py"""
 
 
 class Student:
-    """Student class"""
+    """
+    Class that defines properties of student.
 
+    Attributes:
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
+    """
     def __init__(self, first_name, last_name, age):
-        """init
-        args:
-            first_name : string
-            last_name : string
-            age : int
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """class to json"""
-        attr_dict = self.__dict__
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained in,
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Returns:
+            dict: dictionary representation.
+        """
         if attrs is None:
-            return attr_dict
+            return self.__dict__
 
         new_dict = {}
-        for key in attrs:
-            value = attr_dict.get(key)
-            if value:
-                new_dict[key] = value
+        for item in attrs:
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
         return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance.
+
+        Args:
+            json (dict): json object.
+        """
+        # print("Type json --> {}".format(type(json)))
+        self.__dict__.update(json)
