fbe903954d795be27d527ebd05d6b5df19e91201
staleon-codes <mbaselaleon@gmail.com>
2023-10-10 09:14:36 -0700

    errors fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Add item script."""
-import sys
+"""save args to file"""
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from sys import argv
+
+# load functions
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+FILENAME = "add_item.json"
 
 try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+    json_obj = load_from_json_file(FILENAME)
+    json_obj += argv[1:]
+    save_to_json_file(json_obj, FILENAME)
 
-argc = len(sys.argv)
-
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+except FileNotFoundError:
+    save_to_json_file(argv[1:], FILENAME)
