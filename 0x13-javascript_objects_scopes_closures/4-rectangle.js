17590330479023330437ff1fe6bcb54f11b50772
staleon-codes <mbaselaleon@gmail.com>
2023-12-12 00:11:23 -0800

    mandatory and advanced tasks done

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(
+      ('X'.repeat(this.width) + '\n').repeat(this.height).slice(0, -1)
+    );
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
