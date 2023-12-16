a1e34a15e24d221c624ab751fea632d5304ba08e
staleon-codes <mbaselaleon@gmail.com>
2023-12-16 01:36:48 -0800

    fixed task 8 error

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Task 8 script that prints the first  State object from the database hbtn_0e_6_usa"""
+"""Task 8 script that prints the first
+State object from the database hbtn_0e_6_usa"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
