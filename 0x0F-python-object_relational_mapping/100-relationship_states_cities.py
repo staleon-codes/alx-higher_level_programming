17d046e5e158a71e03ac73b71568ea4118cdd487
staleon-codes <mbaselaleon@gmail.com>
2023-12-16 02:12:51 -0800

    fixed error

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""
-Creates the State "California" with the City "San Francisco" from a DB
-"""
-import sys
-from relationship_state import Base, State
-from relationship_city import City
+"""creates the State California with the ciy San 
+fransisco objects from the database hbtn_0e_100_usa"""
+
+from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+from relationship_state import Base, State
+from relationship_city import Base, City
 
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    newState = State(name='California')
-    newCity = City(name='San Francisco')
-    newState.cities.append(newCity)
+    city = City(name="San Francisco")
+    new_state = State(name="California")
+    new_state.cities.append(city)
 
-    session.add(newState)
-    session.add(newCity)
+    session.add(new_state)
     session.commit()
+    session.close()
