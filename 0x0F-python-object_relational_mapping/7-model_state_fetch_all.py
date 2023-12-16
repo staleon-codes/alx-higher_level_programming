e30d692a161bff8bc66f0c98d5d985f37c4cf3d6
staleon-codes <mbaselaleon@gmail.com>
2023-12-16 01:34:57 -0800

    fixed task 7 error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,18 +2,17 @@
 """Task 7 this script lists all State 
 objects from the database hbtn_0e_6_usa"""
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State)
-
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
