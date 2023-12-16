22b8f2d71cd35eae93b07e99fdb61c72d29e919f
staleon-codes <mbaselaleon@gmail.com>
2023-12-16 02:15:36 -0800

    fixed error

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""lists all State objects, and corresponding city objects, contained in the database hbtn_0e_100_usa"""
+"""lists all State objects, and corresponding city o
+bjects, contained in the database hbtn_0e_100_usa"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, relationship
 from relationship_state import Base, State
 from relationship_city import Base, City
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
