0d6a915c6a75769c502183664036a87a836ba97d
staleon-codes <mbaselaleon@gmail.com>
2023-12-16 01:45:19 -0800

    fixed task 9 error

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Task 10 prints the  State object with the name passed as argument from the database hbtn_0e_6_usa"""
+"""Task 10 prints the State object with the 
+name passed as argument 
+from the database hbtn_0e_6_usa"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
