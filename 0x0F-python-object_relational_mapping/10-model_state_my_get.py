bbba4e20211047e4ff66c115d9d111bfce1cfccc
staleon-codes <mbaselaleon@gmail.com>
2023-12-17 02:50:09 -0800

    fixed code error

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Task 10 prints the State object with the 
-name passed as argument 
-from the database hbtn_0e_6_usa"""
-
+"""Lists all states"""
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query_restult = session.query(State).filter(State.name == argv[4]).first()
-    print("Not found" if query_restult is None else query_restult.id)
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
+    else:
+        print("Not found")
     session.close()
